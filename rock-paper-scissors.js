00f4314438fd3a1158c9d05466386160ad91b50e
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2023-05-11 16:51:21 +0100

    5 round game, winner recorded

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,11 +4,11 @@ function getComputerChoice() {
 
     switch(choice){
         case 1:
-            return("rock")
+            return("rock");
         case 2:
-            return("paper")
+            return("paper");
         case 3:
-            return("scissors")
+            return("scissors");
     };
 };
 
@@ -45,16 +45,16 @@ function playRound(playerSelection, computerSelection) {
                 case "rock":
                     return "Player wins!";
                 case "paper":
-                    return "Draw";
+                    return "Draw!";
                 case "scissors":
-                    return "Computer Wins!";
+                    return "Computer wins!";
             };
         case "scissors":
             switch(computerSelection) {
                 case "rock":
-                    return "Computer Wins!";
+                    return "Computer wins!";
                 case "paper":
-                    return "Player Wins!";
+                    return "Player wins!";
                 case "scissors":
                     return "Draw!";
             };
@@ -62,9 +62,42 @@ function playRound(playerSelection, computerSelection) {
   };
    
 
-const playerSelection = getPlayerChoice();
-// Todo create getPlayerChoice function with validation
 
-const computerSelection = getComputerChoice();
-alert(`Player chose ${playerSelection} and Computer chose ${computerSelection}, ${
-    playRound(playerSelection, computerSelection)}`);
+function game(){
+
+    let playerSelection = "", computerSelection = "";
+    let playerScore = 0, computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection);
+
+        if (result == "Player wins!"){
+            playerScore++
+        } else if (result == "Computer wins!") {
+            computerScore++
+        }
+
+        alert(`Round ${i + 1}: Player chose ${playerSelection} and Computer chose ${computerSelection}, ${result}
+    Score is Player: ${playerScore} - Computer: ${computerScore}`);
+
+    };
+
+    if (playerScore > computerScore){
+        alert(`Player wins the game ${playerScore} to ${computerScore}!`);
+    } else if (computerScore > playerScore){
+        alert(`Computer wins the game ${computerScore} to ${playerScore}!`);
+    } else {
+        alert("It's a draw!");
+    }
+};
+
+game();
+
+// const playerSelection = getPlayerChoice();
+// // Todo create getPlayerChoice function with validation
+
+// const computerSelection = getComputerChoice();
+// alert(`Player chose ${playerSelection} and Computer chose ${computerSelection}, ${
+//     playRound(playerSelection, computerSelection)}`);
